3bd3e6b8109eb5d2950a6e049d49e30f20ae6da0
Igor Q <igorqueirozdossantoslima_@hotmail.com>
2022-10-08 16:59:49 -0300

    Aula async await  fecth e try catch

--- eventos.js
+++ eventos.js
@@ -1,35 +1,53 @@
 function addContato() {
-    console.log(1)
+    // let novaTarefa= document.getElementById('input_nova_tarefa')
+    console.log(input_nova_tarefa.value.split(" "))
 }
 
-AtualizarContatos();
+// atualizarContatos();
 
-function AtualizarContatos() {
-    fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa')// resquet/promise
-        .then(response => response.json())// resposta da resquest/promise
-        .then(body => {// resposta do resposta.json() === corpo da resquest/promise
-            console.log(body)
-            // let tarefas = document.getElementById("tarefas")// forma "normal" de fazer
-            tarefas.innerHTML = "<ul>"
-            body.forEach(pessoa => {
-                tarefas.innerHTML += `<li>${pessoa.nome} - ${pessoa.idade} <button onclick="deletar(${pessoa.id})">Deletar</button></li>`
-            });
-            tarefas.innerHTML += "</ul>"
-        })
+async function atualizarContatos() {
+    // fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa')// resquet/promise
+    //     .then(response => response.json())// resposta da resquest/promise
+    //     .then(body => {// resposta do resposta.json() === corpo da resquest/promise
+    //         console.log(body)
+    //         // let tarefas = document.getElementById("tarefas")// forma "normal" de fazer
+    //         tarefas.innerHTML = "<ul>"
+    //         body.forEach(pessoa => {
+    //             tarefas.innerHTML += `<li>${pessoa.nome} - ${pessoa.idade} <button onclick="deletar(${pessoa.id})">Deletar</button></li>`
+    //         });
+    //         tarefas.innerHTML += "</ul>"
+    //     })
 
+    let response = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa')
+    let body = await response.json()
+    // let tarefas = document.getElementById("tarefas")// forma "normal" de fazer
+    tarefas.innerHTML = "<ul>"
+    body.forEach(pessoa => {
+        tarefas.innerHTML += `<li>${pessoa.nome} - ${pessoa.idade} <button onclick="deletar(${pessoa.id})">Deletar</button></li>`
+    })
+    tarefas.innerHTML += "</ul>"
 }
-
-function deletar(identificador) {
+async function deletar(identificador) {
     console.log(`Deletarei o ${identificador}`)
 
-    fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa/' + identificador, {
+    // fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa/' + identificador, {
+    //     method: 'DELETE',
+    // }).then(res => {
+    //     if (res.ok) {
+    //         console.log('Deu certo')
+    //         AtualizarContatos()
+    //     } else {
+    //         console.log('Deu erro')
+    //     }
+    // })
+
+    let response = fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa/' + identificador, {
         method: 'DELETE',
-    }).then(res => {
-        if (res.ok) {
-            console.log('Deu certo')
-            AtualizarContatos()
-        }else{
-            console.log('Deu erro')
-        }
     })
+    if (response.ok) {
+        atualizarContatos()
+    } else {
+        console.log((await response).statusText)
+    }
+
 }
